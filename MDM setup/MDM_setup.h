a6a49109219f30b4b448e9cb37067b3dbf894dde
TheRooster9674 <149923118+TheRooster9674@users.noreply.github.com>
2024-10-17 10:08:17 -0400

    Initial Commit

---
+++ MDM setup/MDM_setup.h
@@ -0,0 +1,18 @@
+//
+//  MDM_setup.h
+//  MDM setup
+//
+//  Created by D. Love on 10/17/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MDM_setup.
+FOUNDATION_EXPORT double MDM_setupVersionNumber;
+
+//! Project version string for MDM_setup.
+FOUNDATION_EXPORT const unsigned char MDM_setupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDM_setup/PublicHeader.h>
+
+
---
+++ MDM/MDM.h
@@ -0,0 +1,18 @@
+//
+//  MDM.h
+//  MDM
+//
+//  Created by D. Love on 10/17/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MDM.
+FOUNDATION_EXPORT double MDMVersionNumber;
+
+//! Project version string for MDM.
+FOUNDATION_EXPORT const unsigned char MDMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDM/PublicHeader.h>
+
+
